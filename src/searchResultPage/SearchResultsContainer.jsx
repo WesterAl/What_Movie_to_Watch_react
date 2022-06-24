78877b5b3cddce56669473b35af98545dbe00e9b
WesterAl <albin.westerlund@gmail.com>
2022-06-24 19:10:02 +0100

    Added query search e.g. Superman

--- src/searchResultPage/SearchResultsContainer.jsx
+++ src/searchResultPage/SearchResultsContainer.jsx
@@ -1,16 +1,68 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import SearchMovie from '../commonComponents/SearchMovie';
+import IndividualMovieCards from '../homePage/IndividualMovieCards';
 
-function SearchResultsContainer(searchTerm) {
 
-    const location = searchTerm()
-    const { from } = location.state
+function SearchResultsContainer(props) {
+    
 
-    console.log("from the SearchResultsContainer " + searchTerm)
+    const location = useLocation();
+    const searchTerm = location.state;
+
+    //https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher
+
+    const [movies, setMovies] = useState([]);
+
+    const API_URL = 'https://api.themoviedb.org/3'
+
+    //Start URL for getting posters
+    //const Poster_URL_constant = 'http://image.tmdb.org/t/p/w500/'
+
+    //GET popular movies from API
+    
+    const GET_search_movies = "https://api.themoviedb.org/3/search/movie?api_key=" //Jack+Reacher"
+    const API_KEY = '18c103f8d64a085829984a62f7664c81'
+    const queryString = "&query="
+  
+    //Used to load movies from API on page load
+    const fetchData = async () => {
+          const res = await fetch(`${GET_search_movies}${API_KEY}${queryString}${searchTerm.searchTerm}`)
+          const json = await res.json()
+          //console.log(json)
+          setMovies(json.results);
+        };
+
+    useEffect(() => {
+        
+        fetchData();
+      }, []);
+
+    console.log(movies.length)
+    
 
     return (
         <>
-        <h1>SearchResultsContainer start</h1>
-        <h1>SearchResultsContainer end</h1>
+        <SearchMovie />
+        <h1>-------------SearchResultsContainer-----------------</h1>
+        
+        {movies?.length > 0
+                ? (
+                    <div className='movieComponents'>
+                        {movies.map((movie)  => (
+                            <IndividualMovieCards movie={movie} key={movie.id} />
+                            
+                        ))}
+                    </div>
+                ) : (
+                        <div className='empty'>
+                            <h2>No movies</h2>
+
+                        </div>
+                )}
+
+        <h1>-----------SearchResultsContainer-------------------</h1>
         </>
         
     )
