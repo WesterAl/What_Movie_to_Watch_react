117b5963a8a74f78549e15c4165b8858f90e4854
WesterAl <albin.westerlund@gmail.com>
2022-06-27 16:34:11 +0100

    removed double navigation

--- src/header/Navigationbar.jsx
+++ src/header/Navigationbar.jsx
@@ -5,32 +5,14 @@ import { Link } from 'react-router-dom'
 function Navigationbar() {
     return (
         <>
-        <ul className='navMenu'>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-            <Link to="/createlist">Lists</Link>
-            </li>
-            <li>
-            <Link to="/profilepage">Profile</Link>
-            </li>
-            <li>
-            <Link to="/login">Login/Logout</Link>
-            </li>
-            <li>
-            <Link to="/registration">Registrate</Link>
-            </li>
-            <div className='dot'></div>
-        </ul>
-
         <nav className="navMenu">
-            <a href="/">Home</a>
-            <a href="/createlist">Lists</a>
-            <a href="/profilepage">Profile</a>
-            <a href="/login">Login/Logout</a>
-            <a href="/registration">Registrate</a>
-            <div className="dot"></div>
+        <Link to="/">Home</Link>
+        <Link to="/createlist">Lists</Link>
+        <Link to="/profilepage">Profile</Link>
+        <Link to="/login">Login/Logout</Link>
+        <Link to="/registration">Registrate</Link>
+            <div className="dot">
+            </div>
         </nav>
         </>
     )
